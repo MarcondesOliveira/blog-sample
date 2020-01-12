55397785dc74f102ce66b2ce174e41fc9d0b3b18
MarcondesOliveira <marcondescurso@outlook.com>
2020-01-12 00:54:43 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import firebase from './firebase'
+
+import './global.css'
+import Home from './components/Home'
+import Login from './components/Login';
+import Header from './components/Header';
+
+
+class App extends Component {
+
+  state = {
+    firebaseInitialized: false
+  }
+
+  componentDidMount(){
+    firebase.isInitialized().then(resultado => {
+      //devolve o usuário
+      this.setState({firebaseInitialized: resultado})
+    })
+  }
+
+  render() {
+    return this.state.firebaseInitialized !== false ? (
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+        </Switch>
+      </BrowserRouter>
+    ) : (
+      <h1>Carregando...</h1>
+    )
+  }
+}
+
+export default App;
